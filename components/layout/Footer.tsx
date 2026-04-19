d9ac467bb880721f81af089b723aaff1b581ae44
smash-cracker <dennythomas246@gmail.com>
2026-04-19 10:38:19 +0530

    feat: change date

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -20,7 +20,7 @@ export function Footer() {
             maxWidth: 1200,
           }}
         >
-          Get it by <span style={{ fontStyle: "italic", color: accent }}>Today.</span>
+          Get it by <span style={{ fontStyle: "italic", color: accent }}>Tomorrow</span>
         </h3>
         <div
           style={{
